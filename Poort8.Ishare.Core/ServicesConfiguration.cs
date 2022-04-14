96ae1e8047e8d15f1437eab42b8d8c6db914b3d9
Merijn <merijn@poort8.nl>
2022-04-14 22:48:23 +0200

    Added service collection.

---
+++ Poort8.Ishare.Core/ServicesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Poort8.Ishare.Core;
+
+public static class ServicesConfiguration
+{
+    public static void AddIshareCoreServices(this IServiceCollection services)
+    {
+        services.AddSingleton<ICertificateProvider, CertificateProvider>();
+        services.AddSingleton<IAuthenticationService, AuthenticationService>();
+        services.AddSingleton<ISchemeOwnerService, SchemeOwnerService>();
+    }
+}
